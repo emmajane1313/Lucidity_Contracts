42e7d76a0ffaf8101aebef0e0e0b465e96bb4b3b
emmajane1313 <emma.jane@digitalax.xyz>
2025-03-07 18:10:45 -0500

    enlaces

---
+++ contracts/src/LucidityLibrary.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.24;
+
+contract LucidityLibrary {
+    struct Workflow {
+        string workflowMetadata;
+        address creator;
+        uint256 counter;
+    }
+}
---
+++ contracts/src/LucidityWorkflows.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.24;
+
+import "./LucidityLibrary.sol";
+import "./LucidityErrors.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract LucidityWorkflows {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    uint256 public workflowCounter;
+
+    modifier onlyCreator(uint256 counter) {
+        if (!_workflowsToCreator[msg.sender].contains(counter)) {
+            revert LucidityErrors.NotCreator();
+        }
+        _;
+    }
+
+    mapping(uint256 => LucidityLibrary.Workflow) private _workflows;
+    mapping(address => EnumerableSet.UintSet) private _workflowsToCreator;
+
+    event WorkflowCreated(
+        string workflowMetadata,
+        address creator,
+        uint256 counter
+    );
+    event WorkflowDeleted(uint256 counter);
+
+    function createWorkflow(string memory workflowMetadata) public {
+        workflowCounter++;
+
+        _workflows[workflowCounter] = LucidityLibrary.Workflow({
+            workflowMetadata: workflowMetadata,
+            counter: workflowCounter,
+            creator: msg.sender
+        });
+        _workflowsToCreator[msg.sender].add(workflowCounter);
+
+        emit WorkflowCreated(workflowMetadata, msg.sender, workflowCounter);
+    }
+
+    function deleteWorkflow(uint256 counter) public onlyCreator(counter) {
+        delete _workflows[counter];
+
+        _workflowsToCreator[msg.sender].remove(counter);
+
+        emit WorkflowDeleted(counter);
+    }
+
+    function getWorkflowCreator(uint256 counter) public view returns (address) {
+        return _workflows[counter].creator;
+    }
+
+    function getWorkflowMetadata(
+        uint256 counter
+    ) public view returns (string memory) {
+        return _workflows[counter].workflowMetadata;
+    }
+
+    function getCreatorWorkflows(
+        address creator
+    ) public view returns (uint256[] memory) {
+        return _workflowsToCreator[creator].values();
+    }
+}
