da1ef035ed496de710c4e1c845bb05165d3a875d
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-05 19:28:50 -0400

    subgraph

--- lucidity-mainnet/src/lucidity-workflows.ts
+++ lucidity-mainnet/src/lucidity-workflows.ts
@@ -1,33 +1,52 @@
+import { ByteArray, Bytes, store } from "@graphprotocol/graph-ts";
 import {
   WorkflowCreated as WorkflowCreatedEvent,
-  WorkflowDeleted as WorkflowDeletedEvent
-} from "../generated/LucidityWorkflows/LucidityWorkflows"
-import { WorkflowCreated, WorkflowDeleted } from "../generated/schema"
+  WorkflowDeleted as WorkflowDeletedEvent,
+} from "../generated/LucidityWorkflows/LucidityWorkflows";
+import { WorkflowCreated, WorkflowDeleted } from "../generated/schema";
+import { WorkflowMetadata } from "../generated/templates";
 
 export function handleWorkflowCreated(event: WorkflowCreatedEvent): void {
   let entity = new WorkflowCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.workflowMetadata = event.params.workflowMetadata
-  entity.creator = event.params.creator
-  entity.counter = event.params.counter
+    Bytes.fromByteArray(ByteArray.fromBigInt(event.params.counter))
+  );
+  entity.uri = event.params.workflowMetadata;
+  entity.creator = event.params.creator;
+  entity.counter = event.params.counter;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  let ipfsHash = (entity.uri as String).split("/").pop();
+  if (ipfsHash != null) {
+    entity.workflowMetadata = ipfsHash;
+    WorkflowMetadata.create(ipfsHash);
+  }
+
+  entity.save();
 }
 
 export function handleWorkflowDeleted(event: WorkflowDeletedEvent): void {
   let entity = new WorkflowDeleted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.counter = event.params.counter
+  );
+  entity.counter = event.params.counter;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  let entityWorkflow = WorkflowCreated.load(
+    Bytes.fromByteArray(ByteArray.fromBigInt(entity.counter))
+  );
+
+  if (entityWorkflow) {
+    store.remove(
+      "WorkflowCreated",
+      Bytes.fromByteArray(ByteArray.fromBigInt(entity.counter)).toHexString()
+    );
+  }
 }
